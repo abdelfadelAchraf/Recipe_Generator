e6c0e34c9e28978af10622984b25c865f2d06039
abdelfadel achraf <abdelfadelachraf1@gmail.com>
2025-03-15 22:37:09 +0000

    header && home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,8 @@ import Button from "../components/common/button"
 const Home = () => {
   return (
     <div className="flex items-center justify-center text-center flex-col h-screen w-full">
-        <h1 className="text-8xl my-4">Superfast UX Design <br /> <span className="text-red-400 text-6xl"> Powered by AI</span></h1>
-        <p className="text-2xl text-gray-700 mb-6">Design in seconds what used to take weeks.</p>
+        <h1 className="text-8xl my-4">Superfast Recipe Creation<br /> <span className="text-red-400 text-6xl"> Powered by AI</span></h1>
+        <p className="text-2xl text-gray-700 mb-6">Create delicious recipes in seconds—no more guessing ingredients!</p>
         <Button text="start for free" className="py-4 px-16 text-2xl transition-all duration-75"/>
     </div>
   )
